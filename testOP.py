28e2edf4791188e1e51e3c746df0c8de232c0386
blietard <bastien.lietard@outlook.fr>
2022-04-19 16:58:10 +0200

    Updated OP test with new normalisation funcs

--- testOP.py
+++ testOP.py
@@ -1,39 +1,42 @@
-if __name__=='__main__':
+import matplotlib.pyplot as plt 
+import numpy as np
+from tools.utils import *
 
-    import matplotlib.pyplot as plt 
-    import numpy as np
-    from tools.utils import OrthogProcrustAlign, standardize
+factor=2
 
-    factor=2
+step = np.array((-4,-10))
 
-    A = np.random.normal(loc=(15,6),scale=(2,1),size=(60,2))
-    B = factor*(np.array((A[:,1],A[:,0])) ).T
-    B[:,0] = A[:,1].mean()+ (B[:,0] - B[:,0].mean())
+A = np.random.normal(loc=(15,6),scale=(2,1),size=(60,2))
+B = factor*(np.array((A[:,1],A[:,0])) ).T
+B[:,0] = A[:,1].mean()+ (B[:,0] - B[:,0].mean())
+B = B+step
 
-    fig = plt.figure(figsize=(10,10))
-    ax = fig.add_subplot(111)
-    fig.patch.set_facecolor('white')
-    ax.set_facecolor('white')
-    ax.scatter(A.T[0],A.T[1], label='raw A')
-    ax.scatter(B.T[0],B.T[1], label='raw B')
-    plt.axis('equal')
-    plt.title('Raw test data')
-    plt.legend()
-    plt.savefig('OPtest_raw.png',dpi=100,transparent=False)
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111)
+fig.patch.set_facecolor('white')
+ax.set_facecolor('white')
+ax.scatter(A.T[0],A.T[1], label='raw A')
+ax.scatter(B.T[0],B.T[1], label='raw B')
+plt.axis('equal')
+plt.title(f'Raw test data, B scaled with factor {factor} \nand moved by xy step {step}.')
+plt.legend()
+plt.savefig('img_OP/OPtest_raw.png',dpi=100,transparent=False)
 
+std_func = centerunit
 
-    A_std = standardize(A)
-    B_std = standardize(B)
-    ab_align = OrthogProcrustAlign(A_std,B_std)
-    C = B_std.dot(ab_align)
+A_std = std_func(A)
+B_std = std_func(B)
+ab_align = OrthogProcrustAlign(A_std,B_std,standard=True)
+C = B_std.dot(ab_align)
 
-    fig = plt.figure(figsize=(10,10))
-    ax = fig.add_subplot(111)
-    fig.patch.set_facecolor('white')
-    ax.set_facecolor('white')
-    ax.scatter(A_std.T[0],A_std.T[1],lw=4,label='standard A')
-    ax.scatter(B_std.T[0],B_std.T[1],label='standard B')
-    ax.scatter(C.T[0],C.T[1],lw=1,alpha=0.5,c='yellow', label='aligned standard B')
-    plt.title('Aligned test data')
-    plt.legend()
-    plt.savefig('OPtest_aligned.png',dpi=100,transparent=False)
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111)
+fig.patch.set_facecolor('white')
+ax.set_facecolor('white')
+ax.scatter(A_std.T[0],A_std.T[1],lw=4,label='standard A')
+ax.scatter(B_std.T[0],B_std.T[1],label='standard B')
+ax.scatter(C.T[0],C.T[1],lw=1,alpha=0.5,c='yellow', label='aligned standard B')
+plt.title(f'Aligned test data using *{std_func.__name__}*')
+plt.legend()
+plt.savefig('img_OP/OPtest_aligned_{}.png'.format(std_func.__name__),dpi=100,transparent=False)
+
--- tools/utils.py
+++ tools/utils.py
@@ -4,7 +4,29 @@ import numpy as np
 def standardize(arr):
     return ( arr-arr.mean(0)  )/(arr.std(0) )
 
-def OrthogProcrustAlign(arr1,arr2, standard=False, backward=False):
+def unitcenter(arr):
+    # the unit-center normalisation from
+    # https://github.com/Garrafao/LSCDetection/blob/master/modules/embeddings.py
+    arr_c = arr.copy()
+    norms = np.sqrt(np.sum(arr_c**2, axis=1))
+    norms[norms == 0] = 1
+    arr_c /= norms[:, np.newaxis]
+    avg = np.mean(arr_c, axis=0)
+    arr_c -= avg
+    return arr_c
+
+def centerunit(arr):
+    # same as unitcenter but operation order is reversed
+    arr_c = arr.copy()
+    avg = np.mean(arr_c, axis=0)
+    arr_c -= avg
+    norms = np.sqrt(np.sum(arr_c**2, axis=1))
+    norms[norms == 0] = 1
+    arr_c /= norms[:, np.newaxis]
+    return arr_c
+
+
+def OrthogProcrustAlign(arr1,arr2, standard=False, backward=False, std_func=standardize):
     '''
     Return Orthogonal Procrustes alignment matrix of arr1 and arr2.
     `standard` set to True if arr1 and arr2 are already standardized. Default is False.
@@ -13,8 +35,8 @@ def OrthogProcrustAlign(arr1,arr2, standard=False, backward=False):
         A = arr1
         B = arr2
     else:
-        A = standardize(arr1)
-        B = standardize(arr2)
+        A = std_func(arr1)
+        B = std_func(arr2)
 
     temp = B.T @ A
     U, e, Vt = np.linalg.svd(temp,)
